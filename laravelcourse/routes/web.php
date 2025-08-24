8c7b9d4c7ae73ee296639663eeb42cc5d8761d0e
Julian Valencia <valenciajuliann@hotmail.com>
2025-08-24 18:11:05 -0500

    tercer taller

--- laravelcourse/routes/web.php
+++ laravelcourse/routes/web.php
@@ -4,22 +4,30 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-# Home Routes
+// Home Routes
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
 Route::get('/about', 'App\Http\Controllers\HomeController@about')->name('home.about');
 Route::get('/contact', 'App\Http\Controllers\HomeController@contact')->name('home.contact');
 
-# Products Routes
+// Products Routes
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
 Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
 Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
 
-# Cart Routes
-Route::get('/cart', 'App\Http\Controllers\CartController@index')->name("cart.index");
-Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name("cart.add");
-Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name("cart.removeAll");
+// Cart Routes
+Route::get('/cart', 'App\Http\Controllers\CartController@index')->name('cart.index');
+Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name('cart.add');
+Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name('cart.removeAll');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Image Routes
+Route::get('/image', 'App\Http\Controllers\ImageController@index')->name("image.index");
+Route::post('/image/save', 'App\Http\Controllers\ImageController@save')->name("image.save");
+
+Route::get('/image-not-di', 'App\Http\Controllers\ImageNotDIController@index')->name("imagenotdi.index");
+Route::post('/image-not-di/save', 'App\Http\Controllers\ImageNotDIController@save')->name("imagenotdi.save");
+
+
+#Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
